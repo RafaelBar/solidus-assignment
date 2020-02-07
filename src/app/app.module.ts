1e05be68a22d9098d9d082d678624c0e626a6109
Rafael <rafaelbar37@gmail.com>
2020-02-07 13:07:16 +0200

    start the assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyGalleryComponent } from './my-gallery/my-gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyGalleryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-gallery/my-gallery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-gallery',
+  templateUrl: './my-gallery.component.html',
+  styleUrls: ['./my-gallery.component.less']
+})
+export class MyGalleryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
